e171e60150cca3b605f13970e6bf84d24ff379bd
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-12-10 12:18:15 -0500

    initial commit

---
+++ personal-website/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Canvas from './Header';
+import 'bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+        <Canvas />
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ personal-website/src/Test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Test extends React.Component {
+    render(){
+        return(
+            <div>
+                <p className='display-2'>
+                    test
+                </p>
+            </div>
+        );
+    };
+}
+
+export default Test;
