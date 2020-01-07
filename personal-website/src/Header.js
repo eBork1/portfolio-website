bd9ac64c6e5e3288ebd2fe945bd2988b4aaf575f
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-01-07 17:33:34 -0500

    add download link for resume & add titles to links

--- personal-website/src/Header.js
+++ personal-website/src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faBlog } from '@fortawesome/free-solid-svg-icons';
@@ -19,30 +19,30 @@ class Header extends React.Component {
                 </p>
 
                 <div className="row justify-content-center">
-                    <a className="pr-3 pl-3" href="#" id="icons">
-                        <FontAwesomeIcon
-                            icon={faEnvelope}
-                            size="3x"
-                            color="white"
-                        />
-                    </a>
-                    <a className="pr-3 pl-3" href="https://www.linkedin.com/in/ethan-borkosky-764aa6186/" id="icons">
+                    <a className="pr-3 pl-3" href="https://www.linkedin.com/in/ethan-borkosky-764aa6186/" id="icons" title="linkedin">
                         <FontAwesomeIcon
                             icon={faLinkedin}
                             size="3x"
                             color="white"
                         />
                     </a>
-                    <a className="pr-3 pl-3" href="https://github.com/eBork1" id="icons">
+                    <a className="pr-3 pl-3" href="https://github.com/eBork1" id="icons" title="github">
                         <FontAwesomeIcon
                             icon={faGithub}
                             size="3x"
                             color="white"
                         />
                     </a>
-                    <a className="pr-3 pl-3" href="#" id="icons">
+                    <a className="pr-3 pl-3" href="#" id="icons" title="blog- coming soon">
                         <FontAwesomeIcon
                             icon={faBlog}
+                            size="3x"
+                            color="white"
+                        />
+                    </a>
+                    <a className="pr-3 pl-3" href="/ebork_resume.pdf" id="icons" download="ethan-borkosky-resume" title="resume" target='_blank'> 
+                        <FontAwesomeIcon
+                            icon={faFile}
                             size="3x"
                             color="white"
                         />
