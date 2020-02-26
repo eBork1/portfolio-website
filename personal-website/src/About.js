18d2baef4e6bcd0f068132a3cd228f4f56940fe2
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-02-26 14:31:14 -0500

    update styling

--- personal-website/src/About.js
+++ personal-website/src/About.js
@@ -11,30 +11,30 @@ class About extends React.Component {
                 <div className="row">
                     <div className="col-sm-12 col-lg-6 mx-auto mt-4">
                         <Flip bottom big>
-                            <p className="display-3 text-dark">About Me</p>
+                            <p className="display-3 text-dark pb-0 mb-0" id="aboutTitle">About Me</p>
                             <br />
                         </Flip>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-lg-2"></div>
-                    <div className="col-lg-4 col-md-6 col-sm-12">
+                    <div className="col-lg-4 col-md-6 col-sm-12 mx-auto">
                         <Fade bottom>
                             <p className="text-dark">
                                 My Name is Ethan Borkosky and I am a software developer currently living in Lexington, Kentucky.
                                 Aside from software development, my favorite things are music, cooking, and video games. I dabbled with
-                                coding in high school and part of college, but never really dove into it until I decided to put a hold on 
+                                coding in high school and part of college, but never really dove into it until I decided to put a hold on
                                 school and take a 16-week full stack web development bootcamp which was one of the best decisions I have ever made.
-                                I love staying up to date with the dev world and I'm always eager to learn something new. 
+                                I love staying up to date with the dev world and I'm always eager to learn something new.
                             </p>
                         </Fade>
                     </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 mb-5 pb-5">
-                        <Fade bottom>
-                            <img className="img-thumbnail" src={EthanImg} alt="Ethan" />
-                        </Fade>
+                    <div className="row">
+                        <div className="col-lg-4 col-md-6 col-sm-12 mx-auto">
+                            <Fade bottom>
+                                <img className="img-thumbnail" src={EthanImg} alt="Ethan" />
+                            </Fade>
+                        </div>
                     </div>
-                    <div className="col-lg-2"></div>
                 </div>
             </div>
         )
