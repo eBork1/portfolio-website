a6c0ca826a9cd95692fc9b524a0bd76dc1c0af7c
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-02-26 17:24:25 -0500

    remove href attribute for blog logo with no link

--- personal-website/src/Header.js
+++ personal-website/src/Header.js
@@ -19,28 +19,28 @@ class Header extends React.Component {
                 </p>
 
                 <div className="row justify-content-center">
-                    <a className="pr-3 pl-3" href="https://www.linkedin.com/in/ethan-borkosky-764aa6186/" target="_blank" id="icons" title="linkedin">
+                    <a className="pr-3 pl-3" href="https://www.linkedin.com/in/ethan-borkosky-764aa6186/" target="_blank" rel="noopener noreferrer" id="icons" title="linkedin">
                         <FontAwesomeIcon
                             icon={faLinkedin}
                             size="3x"
                             color="white"
                         />
                     </a>
-                    <a className="pr-3 pl-3" href="https://github.com/eBork1" target="_blank" id="icons" title="github">
+                    <a className="pr-3 pl-3" href="https://github.com/eBork1" target="_blank" rel="noopener noreferrer" id="icons" title="github">
                         <FontAwesomeIcon
                             icon={faGithub}
                             size="3x"
                             color="white"
                         />
                     </a>
-                    <a className="pr-3 pl-3" href="#" id="icons" title="blog- coming soon">
+                    <div className="pr-3 pl-3" id="icons" title="blog- coming soon">
                         <FontAwesomeIcon
                             icon={faBlog}
                             size="3x"
                             color="white"
                         />
-                    </a>
-                    <a className="pr-3 pl-3" href="/Ethan_Borkosky_Resume.pdf" target="_blank" id="icons" download="ethan-borkosky-resume" title="resume" target='_blank'>
+                    </div>
+                    <a className="pr-3 pl-3" href="/Ethan_Borkosky_Resume.pdf" target="_blank" rel="noopener noreferrer" id="icons" download="ethan-borkosky-resume" title="resume">
                         <FontAwesomeIcon
                             icon={faFile}
                             size="3x"
