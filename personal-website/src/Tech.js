4e82acac2a29a5ef2198195f3402796e12a9b6f6
Ethan Borkosky <ethan.borkosky@gmail.com>
2020-02-26 17:22:40 -0500

    remove code snippet, update styling

--- personal-website/src/Tech.js
+++ personal-website/src/Tech.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Fade from 'react-reveal/Fade';
 import Flip from 'react-reveal/Flip';
 import Logos from './TechLogos';
-import Sample from './Snippet';
 import './Tech.css';
 
 
 class Tech extends React.Component {
     render() {
         return (
-            <div className="container-fluid" id="techCont">
+            <div>
+            <div className="container" id="techCont">
                 <div className="row">
                     <div className="col-sm-12 col-lg-6 mx-auto mt-4">
                         <Flip bottom big>
@@ -19,39 +19,32 @@ class Tech extends React.Component {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-lg-1"></div>
-                    <div className="col-sm-12 col-lg-5 mt-3">
+                    <div className="col-sm-12 col-lg-5 mx-auto">
                         <Fade bottom>
-
                             <p className="text-white">
                                 Most of my programming experience has been in web development. From the front-end
                                 with React JS to the back-end with Laravel, I've built static websites such as a restaurant
                                 or a political campaign site, and dynamic web-apps such as an automated playlist curator for social events.
                             <br />
                                 <br />
-                                I'm currently working on a single-page web-app that will host a few games and programs that I made in javascript
-                                during my 16-week web development bootcamp.
-                            <br />
-                                <br />
                                 I'm always looking to expand my knowledge of programming, whether it's learning a new language, framework, or development
-                                strategy. I'm currently starting to learn c# with .NET. My ideal job is one in which I get to learn something new and advance myself as
+                                strategy. I'm currently learning c# with .NET. My ideal job is one in which I get to learn something new and advance myself as
                                 a skilled programmer and an articulate thinker.
                             <br />
                                 <br />
                             </p>
                         </Fade>
                     </div>
-                    <div className="col-sm-12 col-lg-5">
-                        <Sample />
-                    </div>
+                    
                 </div>
                 <div className="row">
-                    <div className="col-12">
+                    <div className="col-12" id="logoSection">
                         <Fade bottom>
                             <Logos />
                         </Fade>
                     </div>
                 </div>
+            </div>
             </div>
         )
     }
